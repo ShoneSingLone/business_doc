c665608c37f0e978f436ab1cbee50087a3e2dde5
ShoneSingLone <ShoneSingLone@gmail.com>
2026-03-16 18:32:03 +0800

    ``` refactor(entry): 移除已注释的导入语句
    移除了 entry.vue 文件中被注释掉的两个导入语句，
    清理了不再使用的代码片段。
    ```

--- entry.vue
+++ entry.vue
@@ -5,8 +5,6 @@ export default async function () {
 			size: "small",
 			I18N_LANGUAGE: window.I18N_LANGUAGE
 		})
-		// ,_.$importVue("/common/ui-element/useElementUI.vue", { size: "small", I18N_LANGUAGE: window.I18N_LANGUAGE })
-		// ,_.$importVue("/common/ui-element/useElementUI.NoJS.vue", { size: "small", I18N_LANGUAGE: window.I18N_LANGUAGE })
 	]);
 
 	_.each(
