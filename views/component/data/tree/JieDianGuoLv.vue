e2109840cb73f02affb4e01ca0918bdee47ef185
ShoneSingLone <ShoneSingLone@gmail.com>
2026-03-19 14:42:54 +0800

    ``` fix(tree): 修复树组件过滤方法属性名错误
    将 filter-method 属性更正为 filter-handler，
    以匹配实际的组件API配置并更新相关文档说明。
    ```

--- views/component/data/tree/JieDianGuoLv.vue
+++ views/component/data/tree/JieDianGuoLv.vue
@@ -8,7 +8,7 @@
 			placeholder="Please enter keyword"
 			@input="onQueryChanged" />
 		<xCard style="height: 500px; width: 500px">
-			<xTree ref="treeRef" :data="data" :props="props" :filter-method="filterMethod" />
+			<xTree ref="treeRef" :data="data" :props="props" :filter-handler="filterMethod" />
 		</xCard>
 	</div>
 </template>
@@ -35,7 +35,7 @@ export default async function () {
 				data: this.inject_tree.createData(4, 30, 40),
 				md: `树节点可以过滤
 
-在需要对节点进行过滤时，调用 Tree 实例的 filter 方法， 参数为关键字。 需要注意的是，此时需要设置 filter-method，值为过滤函数。`
+在需要对节点进行过滤时，调用 Tree 实例的 filter 方法， 参数为关键字。 需要注意的是，此时需要设置 filter-handler，值为过滤函数。`
 			};
 		}
 	});
